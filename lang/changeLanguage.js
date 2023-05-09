ef0a9d505c1dd4c9ccf066c310734f554cc0051f
EdgarGrigoryan1996 <grigoryan.edgar.1996@gmail.com>
2023-05-09 04:27:12 +0400

    delete min file compress

--- lang/changeLanguage.js
+++
@@ -1,7 +0,0 @@
-export function changeLanguage(category,languageData){
-    for(let i = 0;i < category.length; i++){
-        category[i].children[0].innerHTML = texts[languageData][i].title
-        category[i].children[1].innerHTML = texts[languageData][i].description
-        category[i].children[2].innerHTML = texts[languageData][i].price
-    }
-}
