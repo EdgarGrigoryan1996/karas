2f9287481ca1a22a8d51532387d7a96897fd3f4e
EdgarGrigoryan1996 <grigoryan.edgar.1996@gmail.com>
2023-05-10 22:53:11 +0400

    add language change

--- js/script.js
+++ js/script.js
@@ -1,4 +1,39 @@
+// Popup change language
+const popup = document.querySelector(".popup")
+const elems = document.querySelectorAll(".popup-lang")
+for(let i = 0; i < elems.length; i++){
+    elems[i].addEventListener("click", () => {
+        // const refresh = window.location.protocol + "//" + window.location.host + window.location.pathname + `?lang=${elems[i].dataset.lang}`;
+        // window.history.pushState({ path: refresh }, '', refresh);
+        localStorage.setItem("lang",lang)
+         window.location = `index.html?lang=${elems[i].dataset.lang}`
+    })
+}
+const showPopupButton = document.querySelector(".showPopupButton");
+showPopupButton.addEventListener("click", showPopup)
+function showPopup() {
+    localStorage.removeItem("lang")
+    location.reload()
+}
 
+// setShowButtonText
+const params = new URLSearchParams(window.location.search);
+let lang = params.get("lang") || "en";
+switch (lang) {
+    case "en":
+        showPopupButton.innerText = "English";
+        break;
+    case "ru":
+        showPopupButton.innerText = "Русский";
+        break;
+    case "am":
+        showPopupButton.innerText = "Հայերեն";
+        break;
+}
+console.log(lang)
+
+
+// -------------
 function changeLanguage(){
     const languages = ["en","am","ru"]
     const params = new URLSearchParams(window.location.search);
@@ -14,6 +49,19 @@ function changeLanguage(){
     document.head.appendChild(script);
 
 }
+const select = document.getElementById('language-select');
+console.log()
+function changeLanguageWithSelect() {
+    const lang = select.value;
+    window.location = `index.html?lang=${lang}`
+}
+
 
 changeLanguage()
+
+if(localStorage.getItem("lang")){
+    popup.style.display = "none"
+} else {
+    popup.style.display = "block"
+}
 
